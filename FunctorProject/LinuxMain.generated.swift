c1491a281c9328aa12bca9d4a20802a55bbd1bd3
jrsonline <jrsonline@me.com>
2018-02-01 23:09:54 -0500

    Add files via upload

---
+++ FunctorProject/LinuxMain.generated.swift
@@ -0,0 +1,11 @@
+// Generated using Sourcery 0.10.0 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+import XCTest
+
+
+// swiftlint:disable trailing_comma
+XCTMain([
+])
+// swiftlint:enable trailing_comma
+
